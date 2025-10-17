ea89b90f437db7dcc57f045af8b79daf7fc0d6b5
Robert Wiedeman <robiedick@gmail.com>
2025-10-17 15:35:43 +0200

    CCJMHomesteading v0.2.3

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { prisma } from "@/lib/prisma";
 import { formatDate } from "@/lib/utils";
 
+export const dynamic = "force-dynamic";
+
 export default async function AdminDashboard() {
   const [articlesCount, publishedCount, categoriesCount, latestArticles] =
     await Promise.all([
